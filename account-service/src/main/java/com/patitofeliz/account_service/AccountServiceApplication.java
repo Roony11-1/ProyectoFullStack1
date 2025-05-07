ef886d387723fe84fc5175b805b5be532bdea7dc
Roony11-1 <eltheasasin@gmail.com>
2025-05-07 14:14:10 -0400

    account-service

--- account-service/src/main/java/com/patitofeliz/account_service/AccountServiceApplication.java
+++ account-service/src/main/java/com/patitofeliz/account_service/AccountServiceApplication.java
@@ -3,6 +3,8 @@ package com.patitofeliz.account_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Alguien salveme!!!
+
 @SpringBootApplication
 public class AccountServiceApplication {
 
