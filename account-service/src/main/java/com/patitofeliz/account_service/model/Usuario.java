92fc41058f8655af0b55bfa22b29f7f9eadbc8a6
Roony11-1 <eltheasasin@gmail.com>
2025-05-07 14:10:20 -0400

    account-service

---
+++ account-service/src/main/java/com/patitofeliz/account_service/model/Usuario.java
@@ -0,0 +1,22 @@
+package com.patitofeliz.account_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Usuario 
+{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+    private String email;
+    private String nombreUsuario;
+    private String password;
+    private String tipoUsuario;
+}
