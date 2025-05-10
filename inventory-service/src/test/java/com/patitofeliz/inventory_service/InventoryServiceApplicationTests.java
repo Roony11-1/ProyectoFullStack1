3ceb89f6b8c056b87a4624f43f369339fd0c9f7d
Roony11-1 <eltheasasin@gmail.com>
2025-05-10 08:53:54 -0400

    -- esqueleto microservicio inventario

---
+++ inventory-service/src/test/java/com/patitofeliz/inventory_service/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patitofeliz.inventory_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
