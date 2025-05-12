0fa73d36c2f90687a4d66727263791f831d5da31
Roony11-1 <eltheasasin@gmail.com>
2025-05-12 09:25:33 -0400

    asd

--- sale-service/src/main/java/com/patitofeliz/sale_service/model/Carrito.java
+++ sale-service/src/main/java/com/patitofeliz/sale_service/model/Carrito.java
@@ -1,6 +1,22 @@
-package main.java.com.patitofeliz.sale_service.model;
+package com.patitofeliz.sale_service.model;
 
+import java.util.List;
+
+import com.patitofeliz.sale_service.model.producto_conexion.Producto;
+import com.patitofeliz.sale_service.model.facturacion.Facturacion;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Carrito 
 {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int usuarioId;
+    private List<Producto> listaProductos; // Agregar producto cuando se haga la conexion
+    private Facturacion Facturacion; // Crea boletas
 }
