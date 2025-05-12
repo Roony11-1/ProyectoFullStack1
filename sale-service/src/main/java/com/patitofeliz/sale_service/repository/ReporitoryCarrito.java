93f844291aa698426527be31493eff11f08d03f8
Roony11-1 <eltheasasin@gmail.com>
2025-05-12 10:09:36 -0400

    subido

--- sale-service/src/main/java/com/patitofeliz/sale_service/repository/ReporitoryCarrito.java
+++ sale-service/src/main/java/com/patitofeliz/sale_service/repository/ReporitoryCarrito.java
@@ -1,13 +1,10 @@
 package com.patitofeliz.sale_service.repository;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.patitofeliz.sale_service.model.Carrito;
 
-import java.util.List;
-
-@repository
+@Repository
 public interface ReporitoryCarrito extends JpaRepository<Carrito,Integer>  {
     Carrito findByUsuarioId(int id);
 
