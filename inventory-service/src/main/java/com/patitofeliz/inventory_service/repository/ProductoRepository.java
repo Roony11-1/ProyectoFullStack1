1e932e83cafd4735035bf41340e558c96d7ff080
Neytan2214 <ney.silva@duocuc.cl>
2025-05-12 09:25:55 -0400

    --cree el repository de producto--

---
+++ inventory-service/src/main/java/com/patitofeliz/inventory_service/repository/ProductoRepository.java
@@ -0,0 +1,13 @@
+package com.patitofeliz.inventory_service.repository;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.patitofeliz.inventory_service.model.Producto;
+import java.util.List;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto,Integer> {
+    List<Producto> findByNombreContainingIgnoreCase(String nombre);
+}
