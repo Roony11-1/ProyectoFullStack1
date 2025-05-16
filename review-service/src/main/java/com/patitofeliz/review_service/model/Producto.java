816ce1a5e617f7055c968c5c8297648ec14e82fc
CHOCOLATIN-12345 <vict.sandovalc@duocuc.cl>
2025-05-16 11:56:47 -0400

    -- lol --

--- review-service/src/main/java/com/patitofeliz/review_service/model/Producto.java
+++ review-service/src/main/java/com/patitofeliz/review_service/model/Producto.java
@@ -1,15 +1,12 @@
 package com.patitofeliz.review_service.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class Producto {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private int id;
     private int productoId;
     private int usuarioId;
