8be0b395a20faebe819c6cc83a2f40f4af040e9b
Roony11-1 <eltheasasin@gmail.com>
2025-05-28 17:55:25 -0400

    -- creado el microservicio de sucursal (esqueleto)

---
+++ branch-service/src/test/java/com/oatitofeliz/branch_service/BranchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oatitofeliz.branch_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BranchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
