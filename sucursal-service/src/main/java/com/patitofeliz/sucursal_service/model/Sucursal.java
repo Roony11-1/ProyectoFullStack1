8ed3bc775937844cbbf3d054b00fc2a6a2c0498c
Roony11-1 <eltheasasin@gmail.com>
2025-06-03 12:40:13 -0400

    -- microservicio sucursal

---
+++ sucursal-service/src/main/java/com/patitofeliz/sucursal_service/model/Sucursal.java
@@ -0,0 +1,18 @@
+package com.patitofeliz.sucursal_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Sucursal 
+{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int id;
+}
