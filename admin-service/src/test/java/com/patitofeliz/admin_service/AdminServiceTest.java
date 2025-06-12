5ad9196a3cfb13605ed200f73db9a6ebc6bff82d
Roony11-1 <eltheasasin@gmail.com>
2025-06-12 16:09:58 -0400

    test

--- admin-service/src/test/java/com/patitofeliz/admin_service/AdminServiceTest.java
+++ admin-service/src/test/java/com/patitofeliz/admin_service/AdminServiceTest.java
@@ -1,17 +1,17 @@
 package com.patitofeliz.admin_service;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.mockito.Mockito.when;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import org.mockito.MockitoAnnotations;
 
 import com.patitofeliz.admin_service.model.Alerta;
@@ -31,28 +31,44 @@ public class AdminServiceTest {
     }
 
     @Test
-    public void TestGetAll(){
-      Alerta u1  = new Alerta();  
-      u1.setId(1);
-      u1.setMensaje("alerta");
-      u1.setTipoAlerta("carrito");
-      u1.setFecha("02/06/2025");
+    public void TestGetAll()
+    {
+        Alerta u1  = new Alerta();  
+        u1.setId(1);
+        u1.setMensaje("alerta");
+        u1.setTipoAlerta("carrito");
+        u1.setFecha("02/06/2025");
 
-      when(alertaRepository.findAll()).thenReturn(Arrays.asList(u1));
+        Alerta u2  = new Alerta();  
+        u2.setId(2);
+        u2.setMensaje("alerta");
+        u2.setTipoAlerta("carrito");
+        u2.setFecha("02/06/2025");
+
+        when(alertaRepository.findAll()).thenReturn(Arrays.asList(u1, u2));
 
 
-      List<Alerta>resultado = alertaSerivce.getAlertas();  
+        List<Alerta>resultado = alertaSerivce.getAlertas();  
       
-       assertEquals(1, resultado.size());
-       assertEquals("alerta", resultado.get(0).getMensaje());
+        assertEquals(2, resultado.size());
+       
+        assertEquals(1, resultado.get(0).getId());
+        assertEquals("alerta", resultado.get(0).getMensaje());
+        assertEquals("carrito", resultado.get(0).getTipoAlerta());
+        assertEquals("02/06/2025", resultado.get(0).getFecha());
 
+        assertEquals(2, resultado.get(1).getId());
+        assertEquals("alerta", resultado.get(1).getMensaje());
+        assertEquals("carrito", resultado.get(1).getTipoAlerta());
+        assertEquals("02/06/2025", resultado.get(1).getFecha());
     }
 
+
     @Test
-    public void testGetUsuarioById_NoExiste(){
+    public void testGetAlertaById_NoExiste(){
         //simularemos que no existe un usuario con el id 99
 
-        when(alertaRepository.findById(1)).thenReturn(Optional.empty());
+        when(alertaRepository.findById(99)).thenReturn(Optional.empty());
 
         //ejecutamos el método
         Alerta resultado = alertaSerivce.getAlerta(1);
@@ -61,25 +77,15 @@ public class AdminServiceTest {
         assertNull(resultado);
     }
 
-
-     @Test
-        public void testGetAlertaById_NoExiste(){
-            //simularemos que no existe un usuario con el id 99
-
-            when(alertaRepository.findById(99)).thenReturn(Optional.empty());
-
-            //ejecutamos el método
-            Alerta resultado = alertaSerivce.getAlerta(1);
-
-            //Verificamos el resultado
-            assertNull(resultado);
-        }
-
     @Test
-    public void testSave(){
+    public void testSave()
+    {
         //Se crea el usuario de prueba para guardar
         Alerta u = new Alerta();
-        u.setMensaje("Carrito");
+        u.setId(1);
+        u.setMensaje("alerta");
+        u.setTipoAlerta("carrito");
+        u.setFecha("02/06/2025");
 
         //debemos simular que el repositorio guarda y retorna el usuario
         when(alertaRepository.save(u)).thenReturn(u);
@@ -88,6 +94,22 @@ public class AdminServiceTest {
         Alerta resultado = alertaRepository.save(u);
 
         //Verificamos que el usuario guardado tenga el nombre correcto
-        assertEquals("Carrito", resultado.getMensaje());
+        assertEquals(1, resultado.getId());
+        assertEquals("alerta", resultado.getMensaje());
+        assertEquals("carrito", resultado.getTipoAlerta());
+        assertEquals("02/06/2025", resultado.getFecha());
     } 
+
+    @Test
+    public void testBorrarAlerta() 
+    {
+        Alerta a = new Alerta();
+        a.setId(1);
+
+        when(alertaRepository.findById(1)).thenReturn(Optional.of(a));
+
+        alertaSerivce.borrar(1);
+
+        verify(alertaRepository).deleteById(1);
+    }
 }
