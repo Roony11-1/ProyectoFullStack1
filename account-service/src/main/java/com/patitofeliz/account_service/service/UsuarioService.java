12fe14fc5340ffb14cd7e460d0ec644b80737692
Roony11-1 <eltheasasin@gmail.com>
2025-06-27 22:59:47 -0400

    a

--- account-service/src/main/java/com/patitofeliz/account_service/service/UsuarioService.java
+++ account-service/src/main/java/com/patitofeliz/account_service/service/UsuarioService.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.patitofeliz.account_service.model.Usuario;
 import com.patitofeliz.account_service.repository.UsuarioRepository;
